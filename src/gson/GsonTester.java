767c55525b961437e57e822508dfb1ab01c590b0
brightlightkim <taeyangk95@naver.com>
2021-09-28 14:13:12 +0900

    adding class

--- src/gson/GsonTester.java
+++ src/gson/GsonTester.java
@@ -1,46 +1,49 @@
 package gson;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class GsonTester {
-    public static void main(String[] args) {
-        String jsonString = "{\"name\":\"Mahesh\", \"age\":21}";
+    public static void main(String args[]) {
 
-        GsonBuilder builder = new GsonBuilder(); //create Gson object using GsonBuilder
-        builder.setPrettyPrinting();
+        GsonTester tester = new GsonTester();
+        try {
+            Student student = new Student();
+            student.setAge(10);
+            student.setName("Mahesh");
+            tester.writeJSON(student);
+            Student student1 = tester.readJSON();
+            System.out.println(student1);
+        }
+        catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void writeJSON(Student student) throws IOException {
+        GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
-        Student student = gson.fromJson(jsonString, Student.class); //deserialize json to object
-        System.out.println(student);
-
-        jsonString = gson.toJson(student); //serialize object to JSON
-        System.out.println(jsonString);
-    }
-}
-
-class Student {
-    private String name;
-    private int age;
-    public Student(){}
-
-    public String getName() {
-        return name;
+        FileWriter writer = new FileWriter("student.json");
+        writer.write(gson.toJson(student));
+        writer.close();
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    private Student readJSON() throws FileNotFoundException {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        BufferedReader bufferedReader = new BufferedReader(
+                new FileReader("student.json"));
 
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String toString() {
-        return "Student [ name: "+name+", age: "+ age+ " ]";
+        Student student = gson.fromJson(bufferedReader, Student.class);
+        return student;
     }
 }
